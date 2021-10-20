b5cf9c4851da22c6b72ab4154db700aa34dcc954
Noah Tigner <nzt@cs.uoregon.edu>
2021-10-20 15:14:33 -0600

    Added Material-UI & Poppins Font

--- nzt-react/src/index.js
+++ nzt-react/src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import logo from './logo.svg';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div className="body">
-      <img src={logo} className="spin" alt="logo" />
-      <p>
-        Hello World
-      </p>
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<div className="body">
+			<img src={logo} className="spin" alt="logo" />
+			<p>
+				Hello World
+			</p>
+		</div>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
