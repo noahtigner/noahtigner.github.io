02d4329b129d9842acf47bfe80c19fcddf6228e4
Noah Tigner <noahzanetigner@gmail.com>
2023-06-18 17:47:56 -0600

    Update temporary Vite placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import vitestLogo from '/vitest.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" width={64} alt="Vite logo" />
+        </a>
+        <a href="https://vitest.dev/" target="_blank">
+          <img
+            src={vitestLogo}
+            className="logo vitest"
+            width={64}
+            alt="Vitest logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            width={64}
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>It's not you, it's me...</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        I'm rebuilding my site with some fun new tools. <br /> Check back soon!
       </p>
     </>
   );
