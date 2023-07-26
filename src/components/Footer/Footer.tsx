8247ba0d96edd1e25f7c17d292452b772f9173d4
Noah Tigner <noahzanetigner@gmail.com>
2023-07-26 16:07:32 -0600

    Improve Button Styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,7 +15,8 @@ const FooterWrapper = styled(Box)(({ theme }) => ({
   marginTop: theme.spacing(2),
   padding: theme.spacing(2),
   [theme.breakpoints.down('sm')]: {
-    padding: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
   },
 }));
 
