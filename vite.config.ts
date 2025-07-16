431dd4bad1d1c7d1f0eda850d15ff24d5b05e6ef
Noah Tigner <noahzanetigner@gmail.com>
2025-07-15 21:19:45 -0600

    Remove unused testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import validateEnvVars from 'validate-env-vars';
@@ -19,11 +19,6 @@ export default defineConfig({
         }),
     },
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['.vitest/setup'],
-  },
   build: {
     rollupOptions: {
       input: {
