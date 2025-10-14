f64334bff048cf7d963bec45c352fd170370b652
Noah Tigner <noahzanetigner@gmail.com>
2025-10-13 21:44:52 -0600

    WIP of react Conf 2025 Article

--- src/components/DividerWithText.tsx
+++ src/components/DividerWithText.tsx
@@ -3,7 +3,7 @@ import { Divider, Typography } from '@mui/material';
 
 export default function DividerWithText({ children }: { children: ReactNode }) {
   return (
-    <Divider>
+    <Divider component="div" role="presentation">
       <Typography variant="h2" sx={{ fontSize: '1.25rem' }}>
         {children}
       </Typography>
