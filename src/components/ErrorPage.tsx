c5f986907a93e47b95c7fba215bf3e045b3a99d2
Noah Tigner <noahzanetigner@gmail.com>
2025-10-22 22:09:05 -0600

    Add basic layout to top-level Layout

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -17,25 +17,27 @@ export default function ErrorPage({
   stack?: string;
 }) {
   return (
-    <span
-      style={{
-        flexGrow: 1,
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '1rem',
-      }}
-    >
-      <span style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-        <h2 style={{ fontSize: '1.5rem', fontWeight: 300 }}>{message}</h2>
-        <StyledSeparator orientation="vertical" />
-        <p style={{ fontSize: '1.5rem', fontWeight: 300 }}>{details}</p>
-        {stack && (
-          <pre>
-            <code>{stack}</code>
-          </pre>
-        )}
+    <>
+      <span
+        style={{
+          flexGrow: 1,
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          gap: '1rem',
+        }}
+      >
+        <span style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+          <h2 style={{ fontSize: '1.5rem', fontWeight: 300 }}>{message}</h2>
+          <StyledSeparator orientation="vertical" />
+          <p style={{ fontSize: '1.5rem', fontWeight: 300 }}>{details}</p>
+        </span>
       </span>
-    </span>
+      {stack && (
+        <pre>
+          <code>{stack}</code>
+        </pre>
+      )}
+    </>
   );
 }
