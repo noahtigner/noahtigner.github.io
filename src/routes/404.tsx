3f16a8f16a2200ae077491ca6b887fb770f10d08
Noah Tigner <noahzanetigner@gmail.com>
2025-10-22 21:38:57 -0600

    Style top-level ErrorBoundary

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,30 +1,7 @@
-import { Separator } from '@base-ui-components/react';
-import styled from '@emotion/styled';
-
-const StyledSeparator = styled(Separator)`
-  width: 1px;
-  height: 2rem;
-  background-color: var(--color-divider);
-`;
+import ErrorPage from '~/components/ErrorPage';
 
 export default function NotFoundRoute() {
   return (
-    <span
-      style={{
-        flexGrow: 1,
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '1rem',
-      }}
-    >
-      <span style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-        <h2 style={{ fontSize: '1.5rem' }}>404</h2>
-        <StyledSeparator orientation="vertical" />
-        <p style={{ fontSize: '1.5rem' }}>
-          The requested page could not be found
-        </p>
-      </span>
-    </span>
+    <ErrorPage message="404" details="The requested page could not be found" />
   );
 }
